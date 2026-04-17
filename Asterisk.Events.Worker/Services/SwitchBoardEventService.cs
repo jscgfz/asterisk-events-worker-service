6dd485cadd947626880bf31db4d26ac6866caeb2
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-04-17 15:41:06 -0500

    Add: jscg | SwitchBoardEventService configuration event producer

--- Asterisk.Events.Worker/Services/SwitchBoardEventService.cs
+++ Asterisk.Events.Worker/Services/SwitchBoardEventService.cs
@@ -1,10 +1,12 @@
-﻿using System.Reactive.Concurrency;
+﻿  using System.Reactive.Concurrency;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
+using System.Text.Json;
 using Asterisk.Events.Worker.Abstractions.Services;
 using Asterisk.Events.Worker.Abstractions.Socket;
 using Asterisk.Events.Worker.Models.Options;
 using Asterisk.Events.Worker.Socket;
+using Confluent.Kafka;
 using Microsoft.Extensions.Options;
 
 namespace Asterisk.Events.Worker.Services;
@@ -15,7 +17,8 @@ internal sealed class SwitchBoardEventService(
   IOptionsMonitor<AmiSerializationOptions> amiOptions,
   ILogger<SwitchBoardEventService> logger,
   ILoggerFactory loggerFactory,
-  ISwitchBoardSenderService sender
+  ISwitchBoardSenderService sender,
+  IProducer<string, string> producer
 ) : IHostedService, ISwitchBoardEventService
 {
   private readonly IOptionsMonitor<SwitchBoard> _options = options;
@@ -27,16 +30,35 @@ internal sealed class SwitchBoardEventService(
   private IAmiConnection _managerConnection = default!;
   private readonly Subject<Dictionary<string, string>> _filterSubject = new();
   private IDisposable? _subscription;
+  private readonly IProducer<string, string> _producer = producer;
 
   public async Task StartAsync(CancellationToken cancellationToken)
   {
     _logger.LogInformation("Initial Configuration {@Configuration}", _options.CurrentValue);
-    await Configure(_options.CurrentValue.EventsConnection);
+    await _producer.ProduceAsync(
+      "configuration",
+      new()
+      {
+        Key = DateTime.Now.ToString("U"),
+        Value = JsonSerializer.Serialize(_options.CurrentValue.EventsConnection.Filters, JsonSerializerOptions.Web)
+      },
+      cancellationToken
+    );
     _options.OnChange(async options =>
     {
       _logger.LogInformation("Configuration changed {@Configuration}", options);
+      await _producer.ProduceAsync(
+      "configuration",
+      new()
+      {
+        Key = DateTime.Now.ToString("U"),
+        Value = JsonSerializer.Serialize(options.EventsConnection.Filters, JsonSerializerOptions.Web)
+      },
+      cancellationToken
+    );
       await Configure(options.EventsConnection);
     });
+    await Configure(_options.CurrentValue.EventsConnection);
   }
 
   private async Task Configure(TcpConnection connection)
